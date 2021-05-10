80b10a94d863e9966dd4aa5df0b2ad5913cf75ec
Jean-Sébastien Hedde <jshedde@scaleway.com>
2021-05-10 18:12:54 +0200

    testing updating resource

--- scaleway/resource_rdb_privilege_test.go
+++ scaleway/resource_rdb_privilege_test.go
@@ -61,6 +61,38 @@ func TestAccScalewayRdbPrivilege_Basic(t *testing.T) {
 					resource.TestCheckResourceAttr("scaleway_rdb_privilege.priv", "permission", "all"),
 				),
 			},
+			{
+				Config: fmt.Sprintf(`
+					resource "scaleway_rdb_instance" "instance" {
+						name = "%s"
+						node_type = "db-dev-s"
+						engine = "PostgreSQL-12"
+						is_ha_cluster = false
+						tags = [ "terraform-test", "scaleway_rdb_user", "minimal" ]
+					}
+
+					resource "scaleway_rdb_database" "db" {
+						instance_id = scaleway_rdb_instance.instance.id
+						name = "foo"
+					}
+
+					resource "scaleway_rdb_user" "foo" {
+						instance_id = scaleway_rdb_instance.instance.id
+						name = "foo"
+						password = "R34lP4sSw#Rd"
+					}
+
+					resource "scaleway_rdb_privilege" "priv" {
+						instance_id   = scaleway_rdb_instance.instance.id
+						user_name     = scaleway_rdb_user.foo.name
+						database_name = scaleway_rdb_database.db.name
+						permission    = "none"
+					}`, instanceName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckRdbPrivilegeExists(tt, "scaleway_rdb_instance.instance", "scaleway_rdb_database.db", "scaleway_rdb_user.foo"),
+					resource.TestCheckResourceAttr("scaleway_rdb_privilege.priv", "permission", "none"),
+				),
+			},
 		},
 	})
 }
